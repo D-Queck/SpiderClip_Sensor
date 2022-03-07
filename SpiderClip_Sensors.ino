e09d7af46393307096bf81fcedab64b162324551
D-Queck <76432409+D-Queck@users.noreply.github.com>
2022-03-07 15:14:13 +0100

    Add files via upload

--- SpiderClip_Sensors.ino
+++ SpiderClip_Sensors.ino
@@ -1,4 +1,4 @@
-//Libraries
+
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <SparkFun_Bio_Sensor_Hub_Library.h>
@@ -8,84 +8,73 @@
 int resPin = 4;
 int mfioPin = 5;
 
-//HeartRate sensor
+
 SparkFun_Bio_Sensor_Hub bioHub(resPin, mfioPin); 
-bioData body;
-
-//MPU  
+bioData body;  
 Adafruit_MPU6050 mpu;
-
-//StepCount
 int steps=0;
 
-//----------------SetUp------------------------
+
 void setup(){
-//MPU
 mpu.begin();
-Serial1.begin(9600);
-Wire.begin();
-
-//HR-sensor
-int result = bioHub.begin();
-int error = bioHub.configBpm(MODE_TWO);
-
-//MPU
-mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial1.begin(9600);
+  Wire.begin();
+  int result = bioHub.begin();
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
   
-delay(100);
+
+  delay(100);
 }
 
-//----------------Loop------------------------
+
 void loop(){
-
-//HR-sensor
-body = bioHub.readBpm();
-
-//MPU
+//initialise and read sensors variables 
 sensors_event_t a, g, temp;
 mpu.getEvent(&a, &g, &temp);
+body = bioHub.readBpm();
 
+//counting steps based on total acceleration
 float totalacc = sqrt(((a.acceleration.x) * (a.acceleration.x)) + ((a.acceleration.y) * (a.acceleration.y)) + ((a.acceleration.z) * (a.acceleration.z)));
-
-if (totalacc>10.4)
+if (totalacc>10)
 {
   steps++;
 }
 
-//SerialPrinting
+//sending the data over the UART1 port (bluetooth) to receive in unity over COM ports ( COM port should be checked from the computer bluetooth settings and inserted in unity serial script
+//COM port "dev" 
+//use json format for the sent data 
 Serial1.print("{");
-
-Serial1.print("\"b\":");
-Serial1.print(body.heartRate);
-Serial1.print(",");
-Serial1.print("\"c\":");
-Serial1.print(body.confidence);
-Serial1.print(",");
-Serial1.print("\"o\":");
-Serial1.print(body.oxygen);
-Serial1.print(",");
 Serial1.print("\"steps\":");
 Serial1.print(steps);
-Serial1.print(",");
-
-Serial1.print("\"ax\":");
-Serial1.print(a.acceleration.x);
-Serial1.print(",");
-
-Serial1.print("\"ay\":");
-Serial1.print(a.acceleration.y);
-Serial1.print(",");
-
-Serial1.print("\"az\":");
-Serial1.print(a.acceleration.z);
-Serial1.print(",");
-
-Serial1.print("\"temp\":");
+Serial1.print(",\"ax\":");
+Serial1.print(a.acceleration.x/10);
+Serial1.print(",\"ay\":");
+Serial1.print(a.acceleration.y/10);
+Serial1.print(",\"az\":");
+Serial1.print(a.acceleration.z/10);
+Serial1.print(",\"rx\":");
+Serial1.print(g.gyro.x);
+Serial1.print(",\"ry\":");
+Serial1.print(g.gyro.y);
+Serial1.print(",\"rz\":");
+Serial1.print(g.gyro.z);
+Serial1.print(",\"temp\":");
 Serial1.print(temp.temperature);
-
+Serial1.print(",\"b\":");
+Serial1.print(body.heartRate);
+Serial1.print(",\"c\":");
+Serial1.print(body.confidence);
+Serial1.print(",\"o\":");
+Serial1.print(body.oxygen);
 Serial1.println("}");
-    
-delay(250); 
+
+
+
+
+
+
+    //increase to lower the bandwidth of data being sent 
+    delay(250); 
 }
